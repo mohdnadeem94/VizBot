25602a14f02bc5f6c31e802886fddfb8e1c4b121
mohdnadeem94 <33385764+mohdnadeem94@users.noreply.github.com>
2019-10-03 01:38:43 -0400

    Commit 10_3_2019_1_38_AM
    Commit 10_3_2019_1_38_AM

--- Vizbot.R
+++ Vizbot.R
@@ -18,11 +18,16 @@ ui <- fluidPage(
         ),
 
         # Show a plot of the generated distribution
+        
         mainPanel(
-            verbatimTextOutput("value"),
-            tableOutput('table'),
-            verbatimTextOutput("basic_stats"),
-            plotOutput("all_plots")
+            tabsetPanel(type = 'tabs',
+                        tabPanel("Basic Analysis",
+                                 verbatimTextOutput("value"),
+                                 tableOutput('table'),
+                                 verbatimTextOutput("basic_stats"),
+                                 plotOutput("all_plots")),
+                        tabPanel("Regression",
+                                 verbatimTextOutput("value_reg")))
         )
     )
 )
@@ -92,7 +97,12 @@ server <- function(input, output) {
                 if(!is.na(inp_plot_fill_final) & !is.na(inp_plot_binw_final) & !is.na(inp_plot_color_final)){
                     g = ggplot(data_set,aes_string(x = inp_plot_x_final))+geom_histogram(binwidth = inp_plot_binw_final,color = inp_plot_color_final,fill = inp_plot_fill_final) +theme_minimal()
                 }
-                
+            }
+            if(one == 'xlimit'){
+                inp_plot_x_lim= unlist(strsplit(input$plot,"xlimit as "))
+                inp_plot_x_lim_final_first = as.numeric(unlist(strsplit(inp_plot_x_lim[2],","))[1])
+                inp_plot_x_lim_final_last = as.numeric(unlist(strsplit(inp_plot_x_lim[2],","))[2])
+                g = g+xlim (inp_plot_x_lim_final_first,inp_plot_x_lim_final_last)
             }
         }
         print(g)
@@ -118,6 +128,36 @@ server <- function(input, output) {
         }
         stat_data
     })
+    
+    reg_fun = reactive({
+        data_set = file_read_fun()
+        column_named = unlist(colnames(data_set))
+        total_words = unlist(strsplit(input$caption," "))
+        for (words_f in total_words ){
+            if(words_f == 'split'){
+                set.seed(123)
+                
+                total_words_sec = unlist(strsplit(input$caption,"train="))
+                reg_train_final = as.numeric(unlist(strsplit(total_words_sec[2]," "))[1])/100
+                reg_test = unlist(strsplit(total_words_sec[2],'test='))
+                reg_test_final = as.numeric(unlist(strsplit(reg_test[2]," "))[1])/100
+                
+                sample_size <- floor(reg_train_final * nrow(data_set))
+                train_ind <- sample(seq_len(nrow(data_set)), size = sample_size)
+                train <- data_set[train_ind, ]
+                test <- data_set[-train_ind, ]
+                
+                reg_y = unlist(strsplit(input$caption,"y as "))
+                reg_y_final = unlist(strsplit(reg_y[2]," "))[1]
+                reg_x = unlist(strsplit(input$caption,"x as "))
+                reg_x_final = unlist(strsplit(reg_x[2]," "))[1]
+                
+                linearMod <- lm(as.formula(paste(reg_y_final,'~',reg_x_final)),data = train )
+                
+            }
+        }
+        print(linearMod)
+    })
 
     output$value <- renderText({ input$caption })
     output$table <- renderTable(
@@ -130,7 +170,7 @@ server <- function(input, output) {
     output$all_plots = renderPlot({
         plot_fun()
     })
-    
+    output$value_reg = renderPrint({ reg_fun( ) })
 }
 
 # Run the application 
